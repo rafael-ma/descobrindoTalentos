116f62df9a5ce4d253e837db0694c47728550666
Rafael Matiola Arceno <96425404+rafael-ma@users.noreply.github.com>
2024-02-07 15:41:34 -0300

    Primeira versão
    Estrutura Base, função geradora de participantes e responsividade.

---
+++ script.js
@@ -0,0 +1,80 @@
+const participantes = [
+    {
+        nome:'Anna Laura Ghisi Gonçalves',
+        cidade: 'Siderópolis',
+        foto: 'assets/participantes/anna-laura-ghisi-goncalves.png'
+    },
+    {
+        nome:'Renata Eduardo Macedo',
+        cidade: 'Jacinto Machado',
+        foto: 'assets/participantes/renata-eduardo-macedo.png'
+    },
+    {
+        nome:'Karol e Sara',
+        cidade: 'Balneário Gaivota',
+        foto: 'assets/participantes/karol-e-sara.png'
+    },
+    {
+        nome:'Jardel Antunes',
+        cidade: 'Criciúma',
+        foto: 'assets/participantes/jardel-antunes.png'
+    },
+    {
+        nome:'Lucas Rocha Varela',
+        cidade: 'Balneário Arroio Do Silva',
+        foto: 'assets/participantes/lucas-rocha-varela.png'
+    },
+    {
+        nome:'Rafael Matiola Arceno',
+        cidade: 'Maracajá',
+        foto: 'assets/participantes/rafael-matiola-arceno.png'
+    },
+    {
+        nome:'Cailane Pereira Torres',
+        cidade: 'Meleiro',
+        foto: 'assets/participantes/cailane-pereira-torres.png'
+    },
+    {
+        nome:'Stéfani Lúcio Albino',
+        cidade: 'Nova Veneza',
+        foto: 'assets/participantes/stefani-lucio-albino.png'
+    },
+    {
+        nome:'José Alfredo',
+        cidade: 'Praia Grande',
+        foto: 'assets/participantes/jose-alfredo.png'
+    },
+    {
+        nome:'Maria Eduarda',
+        cidade: 'Morro da Fumaça',
+        foto: 'assets/participantes/maria-eduarda.png'
+    },
+    
+]
+
+const selectSection = document.querySelector('#select')
+
+participantes.map((element, index) => {
+    const createParticipant = document.createElement('div')
+    createParticipant.setAttribute('id','participante'+index)
+    createParticipant.setAttribute('class','participante')
+    selectSection.appendChild(createParticipant)
+
+    const createImage = document.createElement('img')
+    createImage.setAttribute('src', element.foto)
+    createParticipant.appendChild(createImage)
+
+    const createName = document.createElement('p')
+    createName.setAttribute('class','nome')
+    createName.innerText = element.nome
+    createParticipant.appendChild(createName)
+
+})
+
+function flexAdjust() {
+    if ((participantes.length%3 == 1) && (screen.width > 1000)) {
+        selectSection.lastElementChild.style.marginRight = '530px'
+    } else {
+        selectSection.lastElementChild.style.marginRight = '0'
+    }
+}
