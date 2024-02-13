b9ac61c9811d9381f7c2cbcd2a7ccb15ec3d4aec
Rafael Matiola Arceno <96425404+rafael-ma@users.noreply.github.com>
2024-02-13 19:25:53 -0300

    Monte de coisa

--- script.js
+++ script.js
@@ -51,37 +51,45 @@ const participantes = [
     },
     
 ]
-
 const selectSection = document.querySelector('#select')
 
+/* Generation functions to each participant */
+
 participantes.map((element, index) => {
+    /* Create the parent div for participant */
     const createParticipant = document.createElement('div')
     createParticipant.setAttribute('id','participante'+index)
     createParticipant.setAttribute('class','participante')
     selectSection.appendChild(createParticipant)
 
+    /* Create the image div for the participant  */
     const createImageDiv = document.createElement('div')
     createImageDiv.setAttribute('class','image')
     createParticipant.appendChild(createImageDiv)
 
+    /* Create the image for the participant */
     const createImage = document.createElement('img')
     createImage.setAttribute('src', element.foto)
     createImageDiv.appendChild(createImage)
     
+    /* Create the icon of selected on image div */
     const checkIcon = document.createElement('i')
     checkIcon.setAttribute('class','fa-solid fa-check')
     checkIcon.style.display = 'none'
     createImageDiv.appendChild(checkIcon)
 
+    /* Create the name of participant */
     const createName = document.createElement('p')
     createName.setAttribute('class','nome')
     createName.innerText = element.nome
     createParticipant.appendChild(createName)
 
+    /* Create the city of participant */
     const createCity = document.createElement('small')
     createCity.innerText = element.cidade
     createParticipant.appendChild(createCity)
     
+    /* Add event click to toggle the selected participant */
     createParticipant.addEventListener('click',() => {
         const isSelected = document.querySelector('.selected')
         if (isSelected != null) {
@@ -90,15 +98,10 @@ participantes.map((element, index) => {
         }
         createParticipant.classList.toggle('selected')
         createParticipant.firstElementChild.lastElementChild.style.display = 'block'
-    }
-
-    )
+    })
 })
 
-function nextButton() {
-    const selectedParticipant = document.querySelector('.selected')
-    console.log(selectedParticipant)
-}
+/* Auto adjust in diferent screen sizes to the last participant div*/
 
 function flexAdjust() {
     if ((participantes.length%3 == 1) && (screen.width > 970)) {
@@ -108,3 +111,66 @@ function flexAdjust() {
     }
 }
 
+/* Validation to next button */
+
+function nextButton() {
+    const selectedParticipant = document.querySelector('.selected')
+    if (selectedParticipant == null) {
+        Swal.fire({
+            icon: "error",
+            title: "Oops...",
+            text: "Você precisa selecionar algum participante!",
+        });
+    } else {
+        toggleHiden()
+    }
+}
+
+/* Toggle de the hidden page visualization to form */
+
+function toggleHiden() {
+    const selecteds = [...document.querySelectorAll('.select')]
+    const inputData = document.querySelector('#inputData')
+
+    selecteds.map((element) => {
+        element.style.display = 'none'
+    })
+    inputData.style.display = 'block'
+}
+/* Form and validation */
+const form = document.querySelector('form')
+const nameInput = document.querySelector('#nameInput')
+const lastNameInput = document.querySelector('#lastNameInput')
+const emailInput = document.querySelector('#emailInput')
+
+/* Validate email function */
+function isEmailValid(email) {
+    /* Validate the email with a regex */
+    const emailRegex = new RegExp(/^[a-zA-Z0-9._-]+@[a-zA-Z0-9._-]+\.[a-zA-Z]{2,}$/)
+    return emailRegex.test(email)
+}
+
+/* Submit event */
+
+form.addEventListener('submit', (event) => {
+    event.preventDefault()
+
+    /* Checks empty name */
+    if (nameInput.value === '') {
+        Swal.fire('Por favor, preencha o seu nome.')
+        nameInput.focus()
+        return
+    } else
+    /* Checks empty last name */
+    if (lastNameInput.value === '') {
+        Swal.fire('Por favor, preencha o seu sobrenome.')
+        lastNameInput.focus()
+        return
+    } else 
+    /* Checks empty and invalid email */
+    if (emailInput.value === '' || !isEmailValid(emailInput.value)) {
+        Swal.fire('Por favor, preencha com um email válido.')
+        emailInput.focus()
+        return
+    } 
+})
